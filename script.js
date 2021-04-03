13fc6b3aa965e66be58f0aed8a519a08424274e9
Meeranair1086 <murali.meera@gmail.com>
2021-04-03 09:00:58 -0500

    Final commit for Index.html

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 // TODO: add code here
+window.addEventListener("load", function(){
+    const fetchPromise = fetch("https://handlers.education.launchcode.org/static/astronauts.json");
+    fetchPromise.then(function(response){
+       //debugger;
+        const jsonPromise = response.json();
+             jsonPromise.then(function(json) {
+          
+    const astlist = document.getElementById("container");
+                
+            for(let i=0;i<json.length;i++){
+                astlist.innerHTML+=`<div class="astronaut">
+            <div class="bio">
+            <h3><b>${json[i].firstName} ${json[i].lastName}</b></h3>
+            <ul>
+              <li>hoursInSpace: ${json[i].hoursInSpace}</li>
+              <li>active: ${json[i].active}</li>
+              <li>skills: ${json[i].skills}</li>
+              </ul>   
+              </div>
+              <img class="avatar" src=${json[i].picture}>
+              </div>
+            `;
+        }
+   
+        
+ });
+ 
+}); 
+});
